f1210e7ed5f35ab9f3662139f2c6398e93f5e299
Mahmoud ragab <Mahmoud_ragab@EFinance.com.eg>
2022-11-06 17:56:55 +0200

    # Add unit tests

---
+++ Application.UnitTests/Common/Behaviours/RequestLoggerTests.cs
@@ -0,0 +1,65 @@
+﻿using Candidate_Test_Task.Application.CandidateProfile.Commands.CreateUpdateProfile;
+using Candidate_Test_Task.Application.Common.Behaviours;
+using Candidate_Test_Task.Application.Common.Interfaces;
+using Microsoft.Extensions.Logging;
+using Moq;
+using NUnit.Framework;
+
+namespace Candidate_Test_Task.Application.UnitTests.Common.Behaviours;
+
+public class RequestLoggerTests
+{
+    private Mock<ILogger<CreateUpdateProfileCommand>> _logger = null!;
+    private Mock<ICurrentUserService> _currentUserService = null!;
+    private Mock<IIdentityService> _identityService = null!;
+
+    [SetUp]
+    public void Setup()
+    {
+        _logger = new Mock<ILogger<CreateUpdateProfileCommand>>();
+        _currentUserService = new Mock<ICurrentUserService>();
+        _identityService = new Mock<IIdentityService>();
+    }
+
+    [Test]
+    public async Task ShouldCallGetUserNameAsyncOnceIfAuthenticated()
+    {
+        _currentUserService.Setup(x => x.UserId).Returns(Guid.NewGuid().ToString());
+
+        var requestLogger = new LoggingBehaviour<CreateUpdateProfileCommand>(_logger.Object, _currentUserService.Object, _identityService.Object);
+
+        await requestLogger.Process(new CreateUpdateProfileCommand 
+        {
+            FirstName = "Mahmoud",
+            LastName = "Hassan",
+            Email = "dev_m_hassan@hotmail.com",
+            PhoneNumber = "+201008305188",
+            ScheduledDateTime = DateTime.Now.AddDays(5),
+            LinkedInProfielUrl = "https://www.linkedin.com/in/dev-hassan-2010/",
+            GithubProfielUrl = "https://github.com/dev-hassan",
+            Comment ="Hey sigma"
+        }, new CancellationToken());
+
+        _identityService.Verify(i => i.GetUserNameAsync(It.IsAny<string>()), Times.Once);
+    }
+
+    [Test]
+    public async Task ShouldNotCallGetUserNameAsyncOnceIfUnauthenticated()
+    {
+        var requestLogger = new LoggingBehaviour<CreateUpdateProfileCommand>(_logger.Object, _currentUserService.Object, _identityService.Object);
+
+        await requestLogger.Process(new CreateUpdateProfileCommand
+        {
+            FirstName = "Mahmoud",
+            LastName = "Hassan",
+            Email = "dev_m_hassan@hotmail.com",
+            PhoneNumber = "+201008305188",
+            ScheduledDateTime = DateTime.Now.AddDays(5),
+            LinkedInProfielUrl = "https://www.linkedin.com/in/dev-hassan-2010/",
+            GithubProfielUrl = "https://github.com/dev-hassan",
+            Comment = "Hey sigma"
+        }, new CancellationToken());
+
+        _identityService.Verify(i => i.GetUserNameAsync(It.IsAny<string>()), Times.Never);
+    }
+}
---
+++ Application.UnitTests/Common/Mappings/MappingTests.cs
@@ -0,0 +1,46 @@
+﻿using System.Runtime.Serialization;
+using AutoMapper;
+using Candidate_Test_Task.Application.CandidateProfile.Queries.GetProfiles;
+using Candidate_Test_Task.Application.Common.Mappings;
+using Candidate_Test_Task.Domain.Entities;
+using NUnit.Framework;
+
+namespace Candidate_Test_Task.Application.UnitTests.Common.Mappings;
+
+public class MappingTests
+{
+    private readonly IConfigurationProvider _configuration;
+    private readonly IMapper _mapper;
+
+    public MappingTests()
+    {
+        _configuration = new MapperConfiguration(config => 
+            config.AddProfile<MappingProfile>());
+
+        _mapper = _configuration.CreateMapper();
+    }
+
+    [Test]
+    public void ShouldHaveValidConfiguration()
+    {
+        _configuration.AssertConfigurationIsValid();
+    }
+
+    [Test]
+    [TestCase(typeof(CandidateApplication), typeof(CandidateProfileDto))]
+    public void ShouldSupportMappingFromSourceToDestination(Type source, Type destination)
+    {
+        var instance = GetInstanceOf(source);
+
+        _mapper.Map(instance, source, destination);
+    }
+
+    private object GetInstanceOf(Type type)
+    {
+        if (type.GetConstructor(Type.EmptyTypes) != null)
+            return Activator.CreateInstance(type)!;
+
+        // Type without parameterless constructor
+        return FormatterServices.GetUninitializedObject(type);
+    }
+}
